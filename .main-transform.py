e0a5750c1e09a2a0e43bfb3363c0d7cae6248373
Greentaylor27 <greentaylor18@gmail.com>
2025-04-08 14:46:17 -0500

    OKAY FINALLY HAVE ALL THE CSV FILES CLEANED UP AND HAVE ALL THE DATA I NEED TO THROW INTO MONGO!!!!

--- .main-transform.py
+++
@@ -1,8 +0,0 @@
-from Cleanup import JoysOfPaintingCleanUp
-
-if __name__ == "__main__":
-    cleaner = JoysOfPaintingCleanUp("CSVs")
-    cleaner.load_csv("The Joy Of Painiting - Colors Used")
-    cleaner.clean_episode()
-
-    cleaner.export_episode()
--- Cleanup.py
+++ Cleanup.py
@@ -14,6 +14,21 @@ class JoysOfPaintingCleanUp:
         with open(file_path, mode='r') as file:
             reader = csv.reader(file)
             return list(reader)
+        
+    def extract_episode_id(self):
+        data = self.load_csv('The Joy Of Painiting - Colors Used')
+        header, *rows = data
+
+        episode_ids = []
+
+        for row in rows:
+            season = int(row[4])
+            episode = int(row[5])
+
+            id = f"S{season:02}E{episode:02}"
+            episode_ids.append(id)
+
+        return episode_ids
 
     def clean_date(self, date_str):
         try:
@@ -22,11 +37,11 @@ class JoysOfPaintingCleanUp:
         except ValueError:
             return date_str
 
-    def clean_episode(self):
+    def clean_episode(self, episode_ids):
         clean_list = []
         data = self.load_csv('The Joy Of Painting - Episode Dates')
 
-        for row in data:
+        for idx, row in enumerate(data):
             row_string = "".join(row)
 
             match_title = re.search(r"^(.*?)(?=\()", row_string)
@@ -35,6 +50,7 @@ class JoysOfPaintingCleanUp:
             title = ""
             formatted_date = ""
             note = ""
+            ep_id = episode_ids[idx] if idx < len(episode_ids) else ""
 
             if match_title:
                 title = match_title.group(1).strip()
@@ -50,24 +66,111 @@ class JoysOfPaintingCleanUp:
                     note = after_date_text
 
             if title:
-                clean_list.append([title, formatted_date, note])
+                clean_list.append([ep_id,title, formatted_date, note])
 
         return clean_list
 
-    # def clean_subject(self):
-    # def clean_color(self):
+    def clean_subject(self):
+        data = self.load_csv('The Joy Of Painiting - Subject Matter')
+
+        if not data:
+            return []
+        else:
+            header = data[0]
+
+        subject_matter = header[2:]
+
+        subject_row = [[i + 1, subject] for i, subject in enumerate(subject_matter)]
+
+        return subject_row
+
+    def clean_color(self):
+        color_hex_map = {
+            'Black_Gesso': '#000000-pr', # pr for Primer
+            'Bright_Red': '#FF000D',
+            'Burnt_Umber': '#6E260E',
+            'Dark_Sienna': '#3C1414',
+            'Cadmium_Yellow': '#FDDA0D',
+            'Indian_Red': '#CD5C5C',
+            'Indian_Yellow': '#E3A857',
+            'Liquid_Black': '#000000-pa', # pa for Paint
+            'Liquid_Clear': '#00FFFFFF',
+            'Midnight_Black': '#0D0D0D',
+            'Phthalo_Blue': '#000F89',
+            'Phthalo_Green': '#123524',
+            'Prussian_Blue': '#003153',
+            'Sap_Green': '#507D2A',
+            'Titanium_White': '#E4E4E4',
+            'Van_Dyke_Brown': '#574739',
+            'Yellow_Ochre': '#CC7722',
+            'Alizarin_Crimson': '#E32636'
+        }
+
+        data = self.load_csv('The Joy Of Painiting - Colors Used')
+
+        header = data[0]
+        color_names = header[10:]
+
+        cleaned_colors = []
+
+        for name in color_names:
+            name = name.strip()
+            hex_code = color_hex_map.get(name)
+
+            if hex_code:
+                cleaned_colors.append([
+                    hex_code,
+                    name
+                ])
+            else:
+                print(f"[WARN] No hex code found for: {name}")
+
+        return cleaned_colors
+
 
     def export_episode(self):
 
         output_file = os.path.join(self.folder_path, 'cleaned_episodes.csv')
-        header = ['Episode', 'Date (y-m-day)', 'Notes (optional)']
+        header = ['ID', 'Episode (Title)', 'Date (y-m-day)', 'Notes (Optional)']
+
+        episode_ids = self.extract_episode_id()
+        cleaned_episode = self.clean_episode(episode_ids)
 
         with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(header)
-            writer.writerows(self.clean_episode())
+            writer.writerows(cleaned_episode)
 
-        print('CSV should have been sucessfully written')
+        print('cleaned_episodes.csv should have been sucessfully written')
 
-    # def export_subject(self):
-    # def export_color(self):
+    def export_subject(self):
+        output_file = os.path.join(self.folder_path, 'cleaned_subject.csv')
+        header = ['ID', 'Subject Matter']
+        cleaned_subject = self.clean_subject()
+
+        with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(header)
+            writer.writerows(cleaned_subject)
+
+        print('cleaned_subject.csv should have been sucessfully written')
+
+    def export_color(self):
+        output_file = os.path.join(self.folder_path, 'cleaned_colors.csv')
+        header = ['ID', 'Color']
+        cleaned_color = self.clean_color()
+
+        with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(header)
+            writer.writerows(cleaned_color)
+
+        print('cleaned_color.csv should have been successfully written')
+
+
+if __name__ == "__main__":
+    cleaner = JoysOfPaintingCleanUp("CSVs")
+
+    cleaner.export_color()
+    cleaner.export_episode()
+    cleaner.export_subject()
