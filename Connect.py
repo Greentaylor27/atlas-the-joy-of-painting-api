122df3f0ad80d6d77f899230a3966572399f08f5
Greentaylor27 <greentaylor18@gmail.com>
2025-04-14 00:23:30 -0500

    little late hope this works

--- Connect.py
+++ Connect.py
@@ -5,28 +5,28 @@ import csv
 class JoyOfPaintingConnect(JoyOfCoding):
     def __init__(self, folder_path):
         super().__init__(folder_path)
+        self.color_map = self.build_color_map()
 
     def build_color_map(self):
-        data = self.load_csv('clean_color.csv')
+        data = self.load_csv('cleaned_colors.csv')
         header, *rows = data
         return {row[1].strip(): row[0] for row in rows}
 
     def connect_episode_to_color(self):
         color_meta_data = self.load_csv('The Joy Of Painiting - Colors Used')
         header, *rows = color_meta_data
-        color_name = header[10:]
+        color_headers = header[10:]
 
         episode_ids = self.extract_episode_id()
-
         output_row = []
 
         for episode_id, row in zip(episode_ids, rows):
-            binary_flags = header[10:]
+            binary_flags = row[10:]
 
             for indx, val in enumerate(binary_flags):
                 if val == '1':
-                    color_name = color_name[indx].strip()
-                    color_id = self.build_color_map.get(color_name)
+                    color_name = color_headers[indx].strip()
+                    color_id = self.color_map.get(color_name)
                     if color_id:
                         output_row.append([episode_id, color_id])
                     else:
@@ -34,7 +34,14 @@ class JoyOfPaintingConnect(JoyOfCoding):
         return output_row
 
     def connect_episode_to_subject(self):
-        pass
+        subject_metadata = self.load_csv('The Joy Of Painiting - Subject Matter')
+
+        header, *rows = subject_metadata
+        subject_headers = header[2:]
+
+        output_row = []
+
+
 
     def export_EpisodeColor(self):
         output_file = os.path.join(self.folder_path, 'EpisodeColor.csv')
@@ -52,4 +59,5 @@ class JoyOfPaintingConnect(JoyOfCoding):
 if __name__ == "__main__":
     Connecter = JoyOfPaintingConnect("CSVs")
 
-    Connecter.export_EpisodeColor()
+    # Connecter.export_EpisodeColor()
+    Connecter.connect_episode_to_subject()
--- README.md
+++ README.md
@@ -1,5 +1,36 @@
-# atlas-the-joy-of-painting-api
-ETL (Extract Transform Load)
+# Atlas-the-joy-of-painting-api
+
+learning ETL (Extract, Transform, Load)
 
 
-## Adding something to say I have a commit for the past 4 days
+## ERD model
+
+https://imgur.com/a/rDsWYi7
+
+## Instructions
+
+To explain how to work this and what the intention was since I never finished
+
+1. run Cleanup.py (this is the extract step where I pull all the data I need out of each of the CSVs)
+
+2. Run Connect.py (This was intended to be the point at which we make the junction table in a CSV format to have a hard copy as well as to the database)
+
+this is where I explain the intention:
+
+3. Run Import.py (This step was intended to send all the CSVs to the database (PostgreSQL) I ended up changing architectures mid way through the project)
+
+4. Run all the connecting backend to be able to filter and post what you need
+## Authors
+
+- [Taylor Green](https://www.github.com/Greentaylor27)
+
+
+## Pitfalls
+
+So I feel this section is needed as this project never got finished.
+
+1. Sitting down to draft the ERD diagram for how the tables will layout
+
+2. How to connect the tables. (purpose of this project really)
+
+3. Picking which database archietecture to use (I ended up changing about half way through)
